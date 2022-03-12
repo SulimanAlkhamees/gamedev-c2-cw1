894772e9529768c16d21db71d2d525a9b176156f
SulimanAlkhamees <salooom2006@gmail.com>
2022-03-12 13:55:23 +0300

    summry

---
+++ c2-cw1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "shoto";
+            string superPower1 = "ice";
+            string superPower2 = "fire";
+            int age = 15;
+            double speed = 2.25;
+
+
+
+
+            Console.WriteLine("I am {0} and my age is {1} my super power {2} and {3} my speed is {4}", characterName, age, superPower1, superPower2, speed);
+            
+        }
+    }
+}
